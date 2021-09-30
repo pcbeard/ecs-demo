0877e4c7284b772ccbaf737860d88d503c088b25
pcbeard <pcbeard@mac.com>
2021-09-30 01:12:07 -0700

    Remove dependency on SDL_image

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SDL", url: "git@github.com:ARPGLTD/SDL.git", .branch("SwiftPackage")),
-        .package(name: "SDL_image", url: "git@github.com:ARPGLTD/SDL_image.git", .branch("SwiftPackage")),
         .package(name: "FirebladeECS", url: "https://github.com/fireblade-engine/ecs.git", from: "0.17.4"),
         .package(name: "FirebladeMath", url: "https://github.com/fireblade-engine/math.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "SDLKit", dependencies: ["SDL", "SDL_image"]),
+        .target(name: "SDLKit", dependencies: ["SDL"]),
         .target(
             name: "Particles",
             dependencies: ["FirebladeECS", "SDL", "SDLKit"]),
